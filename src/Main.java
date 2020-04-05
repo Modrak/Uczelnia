77dbf8c650e8d50b3b02ee27daa1aaf1eac12b9c
Eryczek <modrak11@.pl>
2020-04-05 19:21:08 +0200

    dodanie menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,95 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("kk");
+
+        Scanner menu =new Scanner(System.in);
         Uczelnia uczelnia=new Uczelnia();
-        uczelnia.addingAdministration();
+        System.out.println("Co chcesz zrobić: (podaj nr)");
+        System.out.println("1. Dodać pracownika administracji ");
+        System.out.println("2. Dodać nauczyciela ");
+        System.out.println("3. Znaleźć pracownika administracji ");
+        System.out.println("4. Znaleźć nauczyciela ");
+        System.out.println("5. Usunąć pracownika administracji ");
+        System.out.println("6. Usunąć nauczyciela ");
+        int choose=0;
+        try {
+            choose = menu.nextInt();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Podaj cyfre!");
+        }
+        switch (choose){
+            case 1:
+                uczelnia.addingAdministration();
+                break;
+            case 2:
+                uczelnia.addingTeacher();
+                break;
+            case 3:
+                System.out.println("Jak chcesz go znaleźć");
+                System.out.println("1. Po imieniu");
+                System.out.println("2. Po nazwisku");
+                System.out.println("3. Po zarobkach");
+                System.out.println("4. Po ilości rozwiązanych spraw");
+
+                int chooseCase3= menu.nextInt();
+                switch (chooseCase3){
+                    case 1:
+                        uczelnia.lookingForAdminByName();
+                        break;
+                    case 2:
+                        uczelnia.lookingForAdminByLastName();
+                        break;
+                    case 3:
+                       uczelnia.lookingForAdminBySalary();
+                        break;
+                    case 4:
+                        uczelnia.lookingForAdminBySolvedCases();
+                        break;
+                }
+
+                break;
+
+
+            case 4:
+                System.out.println("Jak chcesz go znaleźć");
+                System.out.println("1. Po imieniu");
+                System.out.println("2. Po nazwisku");
+                System.out.println("3. Po zarobkach");
+                System.out.println("4. Po stopniu naukowym");
+                System.out.println("5. Po ilości publikacji");
+
+                int chooseCase4= menu.nextInt();
+                switch (chooseCase4){
+                    case 1:
+                        uczelnia.lookingForTeacherByName();
+                        break;
+                    case 2:
+                        uczelnia.lookingForTeacherByLastName();
+                        break;
+                    case 3:
+                        uczelnia.lookingForTeacherBySalary();
+                        break;
+                    case 4:
+                        uczelnia.lookingForTeacherByDegree();
+                        break;
+                    case 5:
+                        uczelnia.lookingForTeacherBySciPublication();
+                        break;
+                }
+                break;
+            case 5:
+                //usuwanie administracji
+                break;
+            case 6:
+                //usuwanie nauczycieli
+                break;
+
+        }
+
+
+
     }
 }
--- src/Uczelnia.java
+++ src/Uczelnia.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,15 +33,27 @@ public class Uczelnia {
             e.printStackTrace();
         }
 
-
+        System.out.println("Podaj imie:");
         String a=geting.nextLine();
+        System.out.println("Podaj nazwisko:");
         String b=geting.nextLine();
-        int c=geting.nextInt();
-        int d=geting.nextInt();
-        int e=geting.nextInt();
-        int f=geting.nextInt();
+        System.out.println("Podaj wiek:");
+        try {
+            int c = geting.nextInt();
+            System.out.println("Podaj PESEL:");
+            int d = geting.nextInt();
+            System.out.println("Podaj wypłate:");
+            int e = geting.nextInt();
+            System.out.println("Podaj ilość rozwiązanych spraw:");
+            int f = geting.nextInt();
 
-        administration.add(new PracownikAdministracyjny(a,b,c,d,e,f));
+            administration.add(new PracownikAdministracyjny(a,b,c,d,e,f));
+        }
+
+        catch (InputMismatchException e) {
+            System.out.println("Należy podawać liczby.");
+        }
+
 
         try
         {
@@ -100,6 +113,7 @@ public class Uczelnia {
             String tempName=geting.nextLine();
             for(int i=1; i<=administration.size(); i++){
                 if(tempName.equals(administration.get(i-1).name)){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
                     System.out.println("Imie: "+administration.get(i-1).name);
                     System.out.println("Nazwisko: "+administration.get(i-1).lastName);
                     System.out.println("Wiek: "+administration.get(i-1).age);
@@ -135,6 +149,83 @@ public class Uczelnia {
             String tempLastName=geting.nextLine();
             for(int i=1; i<=administration.size(); i++){
                 if(tempLastName.equals(administration.get(i-1).lastName)){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+administration.get(i-1).name);
+                    System.out.println("Nazwisko: "+administration.get(i-1).lastName);
+                    System.out.println("Wiek: "+administration.get(i-1).age);
+                    System.out.println("PESEL: "+administration.get(i-1).pesel);
+                    System.out.println("Wypłata: "+administration.get(i-1).salary);
+                    System.out.println("Rozwiązane sprawy: "+administration.get(i-1).solvedCases);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void lookingForAdminBySalary(){
+
+        try {
+
+            FileInputStream file = new FileInputStream("Administration.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            administration = (ArrayList<PracownikAdministracyjny>) in.readObject();
+            System.out.println("Podaj dolny zakres zarobków:");
+            int bottomSalary=geting.nextInt();
+            System.out.println("Podaj górny zakres zarobków: ");
+            int topSalary=geting.nextInt();
+            for(int i=1; i<=administration.size(); i++){
+                if(bottomSalary<administration.get(i-1).salary) && administration.get(i-1).salary<topSalary){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+administration.get(i-1).name);
+                    System.out.println("Nazwisko: "+administration.get(i-1).lastName);
+                    System.out.println("Wiek: "+administration.get(i-1).age);
+                    System.out.println("PESEL: "+administration.get(i-1).pesel);
+                    System.out.println("Wypłata: "+administration.get(i-1).salary);
+                    System.out.println("Rozwiązane sprawy: "+administration.get(i-1).solvedCases);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void lookingForAdminBySolvedCases(){
+
+        try {
+
+            FileInputStream file = new FileInputStream("Administration.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            administration = (ArrayList<PracownikAdministracyjny>) in.readObject();
+            System.out.println("Podaj dolny zakres zarobków:");
+            int bottomCases=geting.nextInt();
+            System.out.println("Podaj górny zakres zarobków: ");
+            int topCases=geting.nextInt();
+            for(int i=1; i<=administration.size(); i++){
+                if(bottomCases<administration.get(i-1).solvedCases) && administration.get(i-1).solvedCases<topCases){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
                     System.out.println("Imie: "+administration.get(i-1).name);
                     System.out.println("Nazwisko: "+administration.get(i-1).lastName);
                     System.out.println("Wiek: "+administration.get(i-1).age);
@@ -235,6 +326,183 @@ public class Uczelnia {
         }
 
     }
+
+    public void lookingForTeacherByName(){
+        try {
+
+            FileInputStream file = new FileInputStream("Teacher.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            teachers = (ArrayList<PracownikDydaktyczny>) in.readObject();
+            System.out.println("Podaj imie które chcesz znaleźć");
+            String tempName=geting.nextLine();
+            for(int i=1; i<=teachers.size(); i++){
+                if(tempName.equals(teachers.get(i-1).name)){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+teachers.get(i-1).name);
+                    System.out.println("Nazwisko: "+teachers.get(i-1).lastName);
+                    System.out.println("Stopień naukowy: "+ teachers.get(i-1).degree);
+                    System.out.println("Wiek: "+teachers.get(i-1).age);
+                    System.out.println("PESEL: "+teachers.get(i-1).pesel);
+                    System.out.println("Wypłata: "+teachers.get(i-1).salary);
+                    System.out.println("Ilość publikacji: "+teachers.get(i-1).sciPublication);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void lookingForTeacherByLastName(){
+        try {
+
+            FileInputStream file = new FileInputStream("Teacher.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            teachers = (ArrayList<PracownikDydaktyczny>) in.readObject();
+            System.out.println("Podaj nazwisko które chcesz znaleźć");
+            String tempName=geting.nextLine();
+            for(int i=1; i<=teachers.size(); i++){
+                if(tempName.equals(teachers.get(i-1).lastName)){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+teachers.get(i-1).name);
+                    System.out.println("Nazwisko: "+teachers.get(i-1).lastName);
+                    System.out.println("Stopień naukowy: "+ teachers.get(i-1).degree);
+                    System.out.println("Wiek: "+teachers.get(i-1).age);
+                    System.out.println("PESEL: "+teachers.get(i-1).pesel);
+                    System.out.println("Wypłata: "+teachers.get(i-1).salary);
+                    System.out.println("Ilość publikacji: "+teachers.get(i-1).sciPublication);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void lookingForTeacherBySalary(){
+        try {
+
+            FileInputStream file = new FileInputStream("Teacher.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            teachers = (ArrayList<PracownikDydaktyczny>) in.readObject();
+            System.out.println("Podaj dolny zakres zarobków:");
+            int bottomSalary=geting.nextInt();
+            System.out.println("Podaj górny zakres zarobków: ");
+            int topSalary=geting.nextInt();
+            for(int i=1; i<=teachers.size(); i++){
+                if(bottomSalary<teachers.get(i-1).salary && teachers.get(i-1).salary<topSalary){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+teachers.get(i-1).name);
+                    System.out.println("Nazwisko: "+teachers.get(i-1).lastName);
+                    System.out.println("Stopień naukowy: "+ teachers.get(i-1).degree);
+                    System.out.println("Wiek: "+teachers.get(i-1).age);
+                    System.out.println("PESEL: "+teachers.get(i-1).pesel);
+                    System.out.println("Wypłata: "+teachers.get(i-1).salary);
+                    System.out.println("Ilość publikacji: "+teachers.get(i-1).sciPublication);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void lookingForTeacherByDegree(){
+        try {
+
+            FileInputStream file = new FileInputStream("Teacher.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            teachers = (ArrayList<PracownikDydaktyczny>) in.readObject();
+            System.out.println("Podaj stopień naukowy który chcesz znaleźć:");
+            String tempName=geting.nextLine();
+            for(int i=1; i<=teachers.size(); i++){
+                if(tempName.equals(teachers.get(i-1).degree)){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+teachers.get(i-1).name);
+                    System.out.println("Nazwisko: "+teachers.get(i-1).lastName);
+                    System.out.println("Stopień naukowy: "+ teachers.get(i-1).degree);
+                    System.out.println("Wiek: "+teachers.get(i-1).age);
+                    System.out.println("PESEL: "+teachers.get(i-1).pesel);
+                    System.out.println("Wypłata: "+teachers.get(i-1).salary);
+                    System.out.println("Ilość publikacji: "+teachers.get(i-1).sciPublication);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void lookingForTeacherBySciPublication(){
+        try {
+
+            FileInputStream file = new FileInputStream("Teacher.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            teachers = (ArrayList<PracownikDydaktyczny>) in.readObject();
+            System.out.println("Podaj dolny zakres publikacji:");
+            int bottomSciPub=geting.nextInt();
+            System.out.println("Podaj górny zakres publikacji: ");
+            int topSciPub=geting.nextInt();
+            for(int i=1; i<=teachers.size(); i++){
+                if(bottomSciPub<teachers.get(i-1).salary && teachers.get(i-1).salary<topSciPub){
+                    System.out.println("Oto "+i+" znaleziony pracownik");
+                    System.out.println("Imie: "+teachers.get(i-1).name);
+                    System.out.println("Nazwisko: "+teachers.get(i-1).lastName);
+                    System.out.println("Stopień naukowy: "+ teachers.get(i-1).degree);
+                    System.out.println("Wiek: "+teachers.get(i-1).age);
+                    System.out.println("PESEL: "+teachers.get(i-1).pesel);
+                    System.out.println("Wypłata: "+teachers.get(i-1).salary);
+                    System.out.println("Ilość publikacji: "+teachers.get(i-1).sciPublication);
+                    System.out.println("Nr w tabeli: "+i);
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
